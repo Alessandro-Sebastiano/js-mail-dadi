b4eeeadec07aa358cf515d3b03c240a1b47ba8cd
Alessandro-Sebastiano <alesebastiano15@gmail.com>
2022-10-11 18:19:56 +0200

    Dadi-innerHtml

--- js/dadi.js
+++ js/dadi.js
@@ -1,29 +1,40 @@
 'use strict'
 
 
-let userNumber;
-
-let hostNumber;
-
-let players = [userNumber, hostNumber];
-
-console.log(players);
+let players = [];
+let output = document.querySelectorAll('span');
+const btn = document.querySelector('button');
 
 
-for (let i = 0; i < players.length; i++) {
-
-    players[i] = Math.floor(Math.random() * 6);
-
-    console.log(players[i]);
-
-}
+btn.addEventListener('click', () => {
 
 
-if (players[0] > players[1]) {
-    console.log('Hai vinto');
-} else if (players[0] == players[1]) {
-    console.log('Pareggio');
-} else {
-    console.log('Hai Perso');
-}
+    players.push(Math.floor(Math.random() * 6));
+    players.push(Math.floor(Math.random() * 6));
+
+
+    for (let i = 0; i < players.length; i++) {
+
+        for (let o = 0; o < output.length; o++) {
+
+            output[i].innerHTML = players[i];
+
+        }
+
+    }
+
+    console.log(players);
+
+    let result = document.querySelector('h2');
+
+    if (players[0] > players[1]) {
+        result.innerHTML = 'Hai vinto';
+    } else if (players[0] == players[1]) {
+        result.innerHTML = 'Pareggio';
+    } else {
+        result.innerHTML = 'Hai perso';
+    }
+
+
+})
 
